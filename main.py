df4191514200ae94e584f94dc5aa911ef55829a3
Kostya <pavlenkkovkosta@gmail.com>
2024-01-05 15:44:14 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import sqlite3
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+
+
+class Espresso(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.setWindowTitle("Эспрессо")
+        self.connection = sqlite3.connect("coffee.sqlite")
+        self.view_info()
+
+    def view_info(self):
+        result = self.connection.cursor().execute("SELECT * FROM coffee")
+        self.tableWidget.setColumnCount(7)
+        # Заполняем таблицу элементами
+        for i, row in enumerate(result):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+        self.tableWidget.resizeColumnsToContents()
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Espresso()
+    ex.show()
+    sys.exit(app.exec())
